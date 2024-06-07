96165dd3e414bad20b7a8e2feea61cdf3ca1d596
Kacper Żabiński <kacperzab0@wp.pl>
2024-06-07 12:07:10 +0200

    Changed admin panel

--- yii-app/views/site/admin.php
+++ yii-app/views/site/admin.php
@@ -6,4 +6,14 @@ $this->title = 'Admin Page';
 <div class="admin-page">
     <h1>Welcome Admin</h1>
     <p>This is the content for Admin.</p>
+    <br />
+    <h1>Admin Panel</h1>
+    <p>Control 1</p>
+    <p>Control 2</p>
+    <p>Control 3</p>
+    <p>Control 4</p>
+    <p>Control 5</p>
+    <p>Control 6</p>
+    <p>Control 7</p>
+    <p>Control 8</p>
 </div>
